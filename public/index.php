ac4a68f2fbbe5d9afdf49647de8ed8f35addbd94
Fabian Krupinski <github.hdfavo@gmail.com>
2021-03-12 13:44:51 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,17 @@
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="user-scalable=0, initial-scale=1.0">
+        <title>Q1 Info Proj</title>
+
+        <link rel="stylesheet" href="/public_html/assets/css/main.css">
+
+        <script src="/public_html/assets/js/babylon.js"></script>
+    </head>
+    <body>
+        <canvas id="main_canvas"></canvas>
+
+        <script src="/public_html/assets/js/main.js"></script>
+    </body>
+</html>
